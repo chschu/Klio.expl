595797756c2a3b3eaa2a23a1e58ee037c2a686ff
Christian Schuster <christian@dnup.de>
2022-06-19 23:18:27 +0200

    Use X-Forwarded-* and Forwarded headers (set by reverse proxies) to determine request scheme and host

--- api/builder-config/opt/expl/go.mod
+++ api/builder-config/opt/expl/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/jmoiron/sqlx v1.3.1
--- api/builder-config/opt/expl/main.go
+++ api/builder-config/opt/expl/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
@@ -9,6 +10,7 @@ import (
 	"klio/expl/webhook"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -29,11 +31,18 @@ func main() {
 	}(edb)
 	logrus.Info("Database successfully initialized")
 
+	var wrap func(http.Handler) http.Handler
+	if mustLookupUseProxyHeaders() {
+		wrap = handlers.ProxyHeaders
+	} else {
+		wrap = func(h http.Handler) http.Handler { return h }
+	}
+
 	r := mux.NewRouter()
-	r.Handle("/api/add", webhook.NewAddHandler(edb, mustLookupEnv("WEBHOOK_TOKEN_ADD")))
-	r.Handle("/api/expl", webhook.NewExplHandler(edb, mustLookupEnv("WEBHOOK_TOKEN_EXPL"), "/expl/"))
-	r.Handle("/api/del", webhook.NewDelHandler(edb, mustLookupEnv("WEBHOOK_TOKEN_DEL")))
-	r.Handle("/expl/{key:.*}", web.NewExplHandler(edb))
+	r.Handle("/api/add", wrap(webhook.NewAddHandler(edb, mustLookupEnv("WEBHOOK_TOKEN_ADD"))))
+	r.Handle("/api/expl", wrap(webhook.NewExplHandler(edb, mustLookupEnv("WEBHOOK_TOKEN_EXPL"), "/expl/")))
+	r.Handle("/api/del", wrap(webhook.NewDelHandler(edb, mustLookupEnv("WEBHOOK_TOKEN_DEL"))))
+	r.Handle("/expl/{key:.*}", wrap(web.NewExplHandler(edb)))
 
 	logrus.Info("Listening for HTTP connections...")
 	err = http.ListenAndServe(":8000", r)
@@ -42,6 +51,15 @@ func main() {
 	}
 
 	logrus.Info("Shutting down")
+}
+
+func mustLookupUseProxyHeaders() bool {
+	envStr := mustLookupEnv("USE_PROXY_HEADERS")
+	result, err := strconv.ParseBool(envStr)
+	if err != nil {
+		logrus.Fatalf("cannot convert value: %v", err)
+	}
+	return result
 }
 
 func mustLookupEnv(key string) string {
