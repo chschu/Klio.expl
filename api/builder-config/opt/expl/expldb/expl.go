4cbf6664f21d92b3b771967e72896827cd0ba412
Christian Schuster <christian@dnup.de>
2022-06-25 02:53:51 +0200

    Sort Expl results by id

--- api/builder-config/opt/expl/expldb/expl.go
+++ api/builder-config/opt/expl/expldb/expl.go
@@ -6,7 +6,7 @@ import (
 
 func (e *ExplDB) Expl(key string, indexSpec types.IndexSpec) (entries []types.Entry, err error) {
 	irc, params := indexSpecSqlCondition(indexSpec)
-	sql := "SELECT * FROM entry WHERE (" + irc + ") AND key_normalized = NORMALIZE(LOWER(?), NFC)"
+	sql := "SELECT * FROM entry WHERE (" + irc + ") AND key_normalized = NORMALIZE(LOWER(?), NFC) ORDER BY id"
 	params = append(params, key)
 	err = e.db.Select(&entries, e.db.Rebind(sql), params...)
 	if err != nil {
